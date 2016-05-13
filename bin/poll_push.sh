0d825ae6c09c03cb2eb7363293708a09b56d17f4
kensaku komatsu <kensaku.komatsu@gmail.com>
2016-05-13 13:12:07 -0700

    finished poll_push

--- bin/poll_push.sh
+++ bin/poll_push.sh
@@ -5,22 +5,52 @@
 ## monitor master branch
 ## If updated, automatically pull; npm install; then execute forever restart 0
 
-## SECTION1: check updated
+## SECTION1: check app installed
 
 REPO=https://github.com/eastandwest/skyway-box.git
-HOME=/Users/komasshu
 DIR=skyway-box
+MASTERCONF=${HOME}/config.json.master
 
 if [ -d ${HOME}/${DIR} ]; then
   echo "find ${DIR}! Skip clone process";
 else
   echo "can't find ${DIR}. start clone process";
-  cd ${HOME}
-  git clone ${REPO}
-  cd ${DIR}
-  npm install
+  if [ -f ${MASTERCONF} ]; then
+    # clone repository
+    cd ${HOME}
+    git clone ${REPO}
+
+    # install package
+    cd ${DIR}
+    npm install
+
+    # copy config file
+    cp ${MASTERCONF} ${HOME}/${DIR}/conf/config.json
+
+    # run via forever
+    NODE_ENV=production forever start ${HOME}/${DIR}/index.js
+  else
+    echo "can't find ${MASTERCONF}"
+  fi
 fi
 
+if [ ! -d ${HOME}/${DIR} ]; then
+  echo "Error"; exit
+fi
+
+## SECTION2: check updated
+
+cd ${HOME}/${DIR}
+CHECKER=`git fetch origin master|grep "origin\/master"`
+
+## SECTION3: run update commands
 
 
-## SECTION2: run update commands
+if [ "${CHECKER}" = "" ]; then
+  echo "nothing updated"
+else
+  cd ${HOME}/${DIR}
+  git merge
+  npm install
+  forever restart 0
+fi

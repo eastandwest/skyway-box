c91602d0b70b523e351190c687011e84ec418014
Kensaku Komtasu <kensaku.komatsu@gmail.com>
2016-07-26 16:36:09 -0700

    mobile view for folder and media

--- public/skyway-box.build.js
+++ public/skyway-box.build.js
@@ -14235,8 +14235,12 @@
 	      er = arguments[1];
 	      if (er instanceof Error) {
 	        throw er; // Unhandled 'error' event
+	      } else {
+	        // At least give some kind of context to the user
+	        var err = new Error('Uncaught, unspecified "error" event. (' + er + ')');
+	        err.context = er;
+	        throw err;
 	      }
-	      throw TypeError('Uncaught, unspecified "error" event.');
 	    }
 	  }
 	
